a06735b1a54327d273530b1691696e75cb344686
Shaunsheeper <95133937+ShaunSheeper@users.noreply.github.com>
2021-11-27 16:34:08 +0100

    Update scoutmap.py

--- scoutmap.py
+++ scoutmap.py
@@ -1,8 +1,6 @@
 import folium
 from pprint import pprint
-
-from folium.map import Popup
-from mod import*
+from modules import*
 
 basemaps = {
     'Google Maps': folium.TileLayer(
@@ -22,18 +20,17 @@ basemaps = {
 }
 
 scoutmaps = folium.Map(location=[46,2], zoom_start=7, min_zoom=7, control_scale=True)
+basemaps['Google Maps'].add_to(scoutmaps)
 basemaps['Google Satellite'].add_to(scoutmaps)
-
-
-
+scoutmaps.add_child(folium.LayerControl())
 
 
 for _ in range(get_elements()):
     print(_)
-    html = f"{get_people_name(_)} <br> {get_people_phone_number(_)}"
+    html = f"{get_addresse(_)}<br>{get_people_name(_)}<br>{get_people_phone_number(_)}<br>{get_camp_size(_)}<br>{get_summer_camp(_)}"
 
     iframe = folium.IFrame(html)
-    popup = folium.Popup(iframe, min_width=200, max_width=500)
+    popup = folium.Popup(iframe, min_width=300, max_width=500)
                         
     folium.Marker([get_lat_location(_), get_long_location(_)], popup = popup, tooltip=get_addresse(_)).add_to(scoutmaps)
 
